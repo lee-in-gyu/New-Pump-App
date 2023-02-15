b0bcb8b6e0ceabc894865fe8184f25d66285b614
lee in gyu <km2094@naver.com>
2023-02-15 14:39:06 +0900

    Firebase Storage 연동

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
 import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { auth } from "../firebase";
+import { db } from "../firebase";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [steps, setSteps] = useState([]);
 
+  const getStepDb = async () => {
+    const dbSteps = await db.collection("step_info").get();
+    dbSteps.forEach((docs) => {
+      const stepsObject = {
+        ...docs.data(),
+      };
+      setSteps((prev) => [docs.data(), ...prev]);
+      console.log(stepsObject.id);
+    });
+  };
   const handleSignOut = () => {
     auth
       .signOut()
@@ -15,12 +34,28 @@ const HomeScreen = () => {
       .catch((error) => alert(error.message));
   };
 
+  useEffect(() => {
+    getStepDb();
+  }, []);
+
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
   return (
     <View style={styles.container}>
-      <Text>E-mail: {auth.currentUser?.email}</Text>
-      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-        <Text style={styles.buttonText}>로그아웃</Text>
-      </TouchableOpacity>
+      <View>
+        <Text>E-mail: {auth.currentUser?.email}</Text>
+        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+          <Text style={styles.buttonText}>로그아웃</Text>
+        </TouchableOpacity>
+        <FlatList
+          data={steps}
+          renderItem={({ item }) => <Item title={item.title} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 };
@@ -30,20 +65,27 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
   },
   button: {
     backgroundColor: "#0782F9",
     width: "60%",
-    padding: 15,
+    padding: 10,
     borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
+    marginTop: 10,
   },
   buttonText: {
     color: "white",
     fontWeight: "700",
     fontSize: 16,
+  },
+  item: {
+    backgroundColor: "grey",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 20,
   },
 });
